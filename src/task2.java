778b6e334e3e782c9ef297851fbab441dbf6863a
Liza Nikitenko <59203054+Lizanik@users.noreply.github.com>
2020-01-05 18:01:54 +0200

    Update task2.java

--- src/task2.java
+++ src/task2.java
@@ -1,10 +1,13 @@
-public class task2{
+import java.util.Scanner;
+public class data2{
     public static void main(String[]arg) {
         double x, y, a;
-        x=6;
-        y =  x * 0.4536;
-        a = y - (int)y;
-        System.out.println((int)y);
-        System.out.println( a);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        x =  input.nextInt();
+        y = x * 0.4536;
+        a =( y -(int) y )*100;
+        System.out.println((int)y + "kilo");
+        System.out.println((int)a + "gram");
     }
 }
